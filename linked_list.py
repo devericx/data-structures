55809ecfe6945287c7dc501743f5b9b26d4e78fe
devericx <devericx@gmail.com>
2018-11-20 14:00:50 -0500

    Implement remove functions

--- linked_list.py
+++ linked_list.py
@@ -6,30 +6,47 @@ class LinkedList:
     
     def __init__(self, data = None):
         self.head = self.Node(data)
+        self.size = 1
         
     def add_to_head(self, data):
         new = self.Node(data, self.head)
         self.head = new
+        self.size += 1
 
     def add_to_tail(self, data):
         new = self.Node(data)
         tail = self.end()
         tail.next = new
-        
-    def remove(self, data):
-        return
+        self.size += 1
 
     def remove_from_head(self):
-        return
+        if self.size > 1:
+            self.head = self.head.next
+            self.size -= 1
+        else:
+            self.head = None
+            self.size = 0
 
     def remove_from_tail(self):
-        return
+        if self.size > 1:
+            i = self.head
+            while i.next.next != None:
+                i = i.next
+            i.next = None
+            self.size -= 1
+        else:
+            self.head = None
+            self.size = 0
+        
 
     def end(self):
-        i = self.head
-        while i.next != None:
-            i = i.next  
-        return i
+        if self.size > 0:
+            i = self.head
+            while i.next != None:
+                i = i.next  
+            return i
+        else:
+            return None
     
     def print(self):
         i = self.head
@@ -53,5 +70,15 @@ if __name__ == "__main__":
 
     print("[+] Adding node with data = 0 to head of 'list' ...\n")
     list.add_to_head(0)
+
+    list.print()
+
+    print("[+] Removing node from head of 'list' ...\n")
+    list.remove_from_head()
+
+    list.print()
+
+    print("[+] Removing node from tail of 'list' ...\n")
+    list.remove_from_tail()
 
     list.print()
