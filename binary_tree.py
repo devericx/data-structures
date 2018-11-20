3e6343c4e95c465a9ca8f9ac155c3004ea7b8b4b
devericx <devericx@gmail.com>
2018-11-20 17:19:33 -0500

    Add traveral methods

--- binary_tree.py
+++ binary_tree.py
@@ -1,3 +1,5 @@
+from queue import Queue
+
 class BinaryTree:
     def __init__(self, value):
         self.value = value
@@ -19,3 +21,71 @@ class BinaryTree:
             self.right_child = new
         else:
             self.right_child = new
+
+    def pre_order(self):
+        print(self.value)
+
+        if self.left_child:
+            self.left_child.pre_order()
+        
+        if self.right_child:
+            self.right_child.pre_order()
+
+    def in_order(self):
+        if self.left_child:
+            self.left_child.in_order()
+        
+        print(self.value)
+
+        if self.right_child:
+            self.right_child.in_order()
+
+    def post_order(self):
+        if self.left_child:
+            self.left_child.post_order()
+
+        if self.right_child:
+            self.right_child.post_order()
+        
+        print(self.value)
+
+    def bfs(self):
+        queue = Queue()
+        queue.put(self)
+
+        while not queue.empty():
+            current_node = queue.get()
+            print(current_node.value)
+
+            if current_node.left_child:
+                queue.put(current_node.left_child)
+            
+            if current_node.right_child:
+                queue.put(current_node.right_child)
+    
+if __name__ == "__main__":
+    root = BinaryTree(1)
+    root.insert_left(2)
+    root.insert_right(5)
+
+    root.left_child.insert_left(3)
+    root.left_child.insert_right(4)
+
+    root.right_child.insert_left(6)
+    root.right_child.insert_right(7)
+
+    print("Pre-order:")
+    root.pre_order()
+    print()
+
+    print("In-order:")
+    root.in_order()
+    print()
+
+    print("Post-order:")
+    root.post_order()
+    print()
+
+    print("BFS:")
+    root.bfs()
+    print()
